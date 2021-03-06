620a210ae2f611d0237d87780117e528e8081c96
Nothelse <nothelse@protonmail.com>
2021-03-06 12:40:14 +0100

    Update 1.2 / Fix checkbox and remove some ambiguous characters

--- popup/pop_passgen.js
+++ popup/pop_passgen.js
@@ -1,7 +1,5 @@
-
-
-var check_symbols = document.getElementById('symbols');
 var check_numbers = document.getElementById('numbers');
+var check_symbols = document.getElementById('symbols');
 var gener = document.getElementById('generate');
 var len_password = document.getElementById('len_pass');
 var zonetext = document.getElementById('zonetext');
@@ -9,12 +7,16 @@ var genbutt = document.getElementById('generate');
 var output = document.getElementById("screen_len");
 output.innerHTML = len_password.value;
 
+let pattern = /[a-hjkm-zA-HJKM-Z]/
+
 len_password.oninput = function() {
   output.innerHTML = this.value;
 }
 
+len_password.onchange = function () {
+  document.getElementById("zonetext").value = Password.generate(output.value);
+}
 
-// permet de mettre le texte dans zonetext
 document.getElementById("generate").addEventListener("click", function() {
   document.getElementById("zonetext").value = Password.generate(output.value);
 });
@@ -25,47 +27,57 @@ document.getElementById("copy").addEventListener("click", function(){
   alert("Text Copied !");
 })
 
-var Password = {
+check_symbols.addEventListener('change', function() {
+  let temp = Password._pattern.source.replace('[','').replace(']','')
+  if (this.checked) {
+    temp = temp + '_$%ù()=\\-+.*!:'
+    Password._pattern = new RegExp(`[${temp}]`)
+  } else {
+    temp = temp.split('_$%ù()=\\-+.*!:').join('')
+    Password._pattern = new RegExp(`[${temp}]`)
+  }
+});
 
-  _pattern : /[a-zA-Z0-9_$%ù()=\-+.*!:]/,
+check_numbers.addEventListener('change', function() {
+  let temp = Password._pattern.source.replace('[','').replace(']','')
+  if (this.checked) {
+    temp = temp + '0-9'
+    Password._pattern = new RegExp(`[${temp}]`)
+  } else {
+    temp = temp.split('0-9').join('')
+    Password._pattern = new RegExp(`[${temp}]`)
+  }
+});
 
-
-  _getRandomByte : function()
-  {
-    if(window.crypto && window.crypto.getRandomValues)
-    {
+let Password = {
+  _pattern : pattern,
+  
+  _getRandomByte : function(){
+    if(window.crypto && window.crypto.getRandomValues){
       var result = new Uint8Array(1);
       window.crypto.getRandomValues(result);
       return result[0];
-    }
-    else if(window.msCrypto && window.msCrypto.getRandomValues)
-    {
+    }else if(window.msCrypto && window.msCrypto.getRandomValues){
       var result = new Uint8Array(1);
       window.msCrypto.getRandomValues(result);
       return result[0];
-    }
-    else
-    {
+    }else{
       return Math.floor(Math.random() * 256);
     }
   },
 
-  generate : function(length)
-  {
+  generate : function(length){
     return Array.apply(null, {'length': length})
-      .map(function()
-      {
+      .map(function(){
         var result;
-        while(true)
-        {
+        while(true){
           result = String.fromCharCode(this._getRandomByte());
-          if(this._pattern.test(result))
-          {
+          if(this._pattern.test(result)){
             return result;
           }
         }
       }, this)
       .join('');
   }
-
 };
+
