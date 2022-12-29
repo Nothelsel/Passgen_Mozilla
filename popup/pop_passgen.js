8d054eb5abb95c1c350447a251e5f9d993bbfed6
nope <nope@vmi843734.contaboserver.net>
2022-12-29 01:09:21 +0100

    Modifications: mise à jour vers la 2.0, ajout de l'historique, legeres corrections graphiques

--- popup/pop_passgen.js
+++ popup/pop_passgen.js
@@ -1,64 +1,24 @@
-var check_numbers = document.getElementById('numbers');
-var check_symbols = document.getElementById('symbols');
-var gener = document.getElementById('generate');
-var len_password = document.getElementById('len_pass');
-var zonetext = document.getElementById('zonetext');
-var genbutt = document.getElementById('generate');
-var output = document.getElementById("screen_len");
-output.innerHTML = len_password.value;
+const check_numbers = document.getElementById('numbers');
+const check_symbols = document.getElementById('symbols');
+const gener = document.getElementById('generate');
+const len_password = document.getElementById('len_pass');
+const zonetext = document.getElementById('zonetext');
+const genbutt = document.getElementById('generate');
+const copybutt = document.getElementById('copy');
+const output = document.getElementById("screen_len");
+
 
 let pattern = /[a-hjkm-zA-HJKM-Z]/
-
-len_password.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-len_password.onchange = function () {
-  document.getElementById("zonetext").value = Password.generate(output.value);
-}
-
-document.getElementById("generate").addEventListener("click", function() {
-  document.getElementById("zonetext").value = Password.generate(output.value);
-});
-
-document.getElementById("copy").addEventListener("click", function(){
-  zonetext.select();
-  document.execCommand("copy");
-  alert("Text Copied !");
-})
-
-check_symbols.addEventListener('change', function() {
-  let temp = Password._pattern.source.replace('[','').replace(']','')
-  if (this.checked) {
-    temp = temp + '_$%ù()=\\-+.*!:'
-    Password._pattern = new RegExp(`[${temp}]`)
-  } else {
-    temp = temp.split('_$%ù()=\\-+.*!:').join('')
-    Password._pattern = new RegExp(`[${temp}]`)
-  }
-});
-
-check_numbers.addEventListener('change', function() {
-  let temp = Password._pattern.source.replace('[','').replace(']','')
-  if (this.checked) {
-    temp = temp + '0-9'
-    Password._pattern = new RegExp(`[${temp}]`)
-  } else {
-    temp = temp.split('0-9').join('')
-    Password._pattern = new RegExp(`[${temp}]`)
-  }
-});
-
 let Password = {
   _pattern : pattern,
   
   _getRandomByte : function(){
     if(window.crypto && window.crypto.getRandomValues){
-      var result = new Uint8Array(1);
+      const result = new Uint8Array(1);
       window.crypto.getRandomValues(result);
       return result[0];
     }else if(window.msCrypto && window.msCrypto.getRandomValues){
-      var result = new Uint8Array(1);
+      const result = new Uint8Array(1);
       window.msCrypto.getRandomValues(result);
       return result[0];
     }else{
@@ -69,10 +29,18 @@ let Password = {
   generate : function(length){
     return Array.apply(null, {'length': length})
       .map(function(){
-        var result;
+        let result;
         while(true){
           result = String.fromCharCode(this._getRandomByte());
           if(this._pattern.test(result)){
+            let data = readStorage('history');
+            let date = new Date();
+            const tmp = {
+              "date": date,
+              "password": result
+            }
+            data.passwords.push(tmp);
+            writeStorage('history', data);
             return result;
           }
         }
@@ -81,3 +49,141 @@ let Password = {
   }
 };
 
+function setDefault() {
+  let regxp = Password._pattern.source.replace('[','').replace(']','')
+  regxp = regxp + '_$%ù()=\\-+.*!:'
+  regxp = regxp + '0-9'
+  Password._pattern = new RegExp(`[${regxp}]`)
+  document.getElementById('symbols').checked = true;
+  document.getElementById('numbers').checked = true;
+  document.getElementById("zonetext").value = Password.generate(output.value);
+}
+
+function createStorage(name) {
+  window.browser.storage.local.set({[name]: {passwords: []}});
+}
+
+function readStorage(name){
+  let storageItem = window.browser.storage.local.get(name);
+  if(storageItem) return storageItem;
+  createStorage(name);
+}
+
+function writeStorage(name, data){
+  this.browser.storage.local.set({[name]: data});
+}
+
+
+function processData(data) {
+  let tmp = ""
+  for(let i = 0; i < data.length; i++) {
+    tmp += `
+    <div class="card mt-1">
+      <div class="card-body">
+       <div class="row">
+        <div class="col">
+         <p class="card-text">${data[i].date}</p>
+        </div>
+        <div class="col">
+          <p class="card-text">${data[i].password}</p>
+        </div>
+       </div>
+      </div>
+    </div>
+    `
+  }
+  return tmp;
+}
+
+
+function init(){
+  output.innerHTML = len_password.value;
+  len_password.oninput = function() {
+    output.innerHTML = this.value;
+  }
+  
+  len_password.onchange = function () {
+    document.getElementById("zonetext").value = Password.generate(output.value);
+  }
+  
+  document.getElementById("generate").addEventListener("click", function() {
+    document.getElementById("zonetext").value = Password.generate(output.value);
+    genbutt.textContent = "GEN ✅";
+    setTimeout(() => {
+      genbutt.textContent = "GEN 🔑";
+    }, 2000);
+  });
+  
+  document.getElementById("copy").addEventListener("click", function(){
+    zonetext.select();
+    document.execCommand("copy");
+    copybutt.textContent = "Copied!";
+  })
+
+  document.getElementById("configBtn").addEventListener("click", function(){
+    if(document.getElementById("modalConf").innerHTML != ""){
+      document.getElementById("modalConf").innerHTML = ``;
+      return;
+    }else{
+      document.getElementById("modalConf").innerHTML = `
+      <div class="modal-content customModal">
+        <h5 class="modal-title mt-2">Configuration ⚙️</h5>
+        <div class="modal-body">
+        <label>Conservation de l'historique :
+          <select class="form-select timeHistory mt-1" name="timeHistory">
+            <option selected value="1"> 1 jour</option>
+            <option value="30">30 jours</option>
+            <option value="90">90 jours</option>
+          </select>
+        </label>
+        </div>
+      </div>
+      `
+    }
+  })
+
+
+  document.getElementById("historyBtn").addEventListener("click", function(){
+    if(document.getElementById("modalConf").innerHTML != ""){
+      document.getElementById("modalConf").innerHTML = ``;
+      return;
+    }else{
+      const data = readStorage('history');
+      console.log(data);
+      const processedData = processData(data);
+      document.getElementById("modalConf").innerHTML = `
+      <div class="modal-content customModal">
+      <h5 class="modal-title mt-2">Historiques 📁</h5>
+        <div class="modal-body">
+        
+        </div>
+      </div>
+    `}
+  })
+  
+  check_symbols.addEventListener('change', function() {
+    let temp = Password._pattern.source.replace('[','').replace(']','')
+    if (this.checked) {
+      temp = temp + '_$%ù()=\\-+.*!:'
+      Password._pattern = new RegExp(`[${temp}]`)
+    } else {
+      temp = temp.split('_$%ù()=\\-+.*!:').join('')
+      Password._pattern = new RegExp(`[${temp}]`)
+    }
+  });
+  
+  check_numbers.addEventListener('change', function() {
+    let temp = Password._pattern.source.replace('[','').replace(']','')
+    if (this.checked) {
+      temp = temp + '0-9'
+      Password._pattern = new RegExp(`[${temp}]`)
+    } else {
+      temp = temp.split('0-9').join('')
+      Password._pattern = new RegExp(`[${temp}]`)
+    }
+  });
+
+  setDefault()
+}
+
+window.addEventListener("load", init, false);
